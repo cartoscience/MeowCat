8021c3fbb842bc026f87e4804b7e3ea19cf4f3e7
BP <32749109+cartoscience@users.noreply.github.com>
2018-11-28 12:24:18 -0500

    Update meowCat.R

--- R/meowCat.R
+++ R/meowCat.R
@@ -5,9 +5,14 @@
 #' @param number.or.pets enter a whole number from 1-100 to give the cat some pets
 #' @return for every pet, our cat friend will meow!
 #' @export
+#'
+#' don't forget to install.packages("cowsay")
+
+library("cowsay")
 
 meowR <- function(number.of.pets) {
   for (i in 1:number.of.pets) {
     print("MEeeeeooooooooW!")
   }
+  say("thank you, human", "cat")
 }
